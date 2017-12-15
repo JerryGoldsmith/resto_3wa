4a2529ee5b75bd476f725e84bac3aca9f276f962
inconnu <exemple@dfg.com>
2017-12-15 16:14:36 +0100

    Récupération des données saisies dans le formulaire par l'utilisateur (httpPostMethod) puis hydratatiion du formulaire (httpGetMethod)

--- application/controllers/booking/BookingController.class.php
+++ application/controllers/booking/BookingController.class.php
@@ -5,32 +5,47 @@
 class BookingController
 {
     public function httpGetMethod(Http $http, array $queryFields)
-    {
-        $inst = new BookingModel(new Database());
+    {   //on crée une instance de la classe bookingModel
+        $bookingModel = new BookingModel(new Database());
+        //on crée une instance de la classe bookingForm pour hydrater le formulaire
+        $bookingForm = new BookingForm();
         //on retourne dans la vue la liste déroulante pour les horaires de réservation et le message d'erreur du flashbag défini dans la methode httpPostMethod ci-dessous
         return [
-            "hoursList" => $inst->displayHourList(),
-            "errorForm" => new FlashBag ()
+            "hoursList" => $bookingModel->displayHourList(),
+            "errorForm" => new FlashBag (),
+            "_form" => $bookingForm
         ];
     }
 
     public function httpPostMethod(Http $http, array $formFields)
     {
-         $inst = new BookingModel(new Database());
+         $bookingModel = new BookingModel(new Database());
          //on vérifie que tous les champs du formulaire ne sont pas vide
         if ($formFields["customerName"]!= "" && $formFields["customerPhone"] != "" && $formFields["bookingDate"] != "" && $formFields["placeNumber"] && $formFields["bookingHour"]) {
             //si oui on exécute la requête d'enregistrement
-            $inst->addBooking([$formFields["customerName"], $formFields["customerPhone"], $formFields["bookingDate"], $formFields["placeNumber"], $formFields["bookingHour"]]);
+            $bookingModel->addBooking([$formFields["customerName"], $formFields["customerPhone"], $formFields["bookingDate"], $formFields["placeNumber"], $formFields["bookingHour"]]);
             //puis on redirige vers la page d'accueil
             $http->redirectTo('/');
 
         } else {
             //si au moins un champ du formulaire est vide on crée un objet flashbag
             $message = new FlashBag ();
+
             //on ajoute le message d'erreur
             $message->add("Tous les champs sont obligatoires");
-            //puis on redirige vers la même page sur laquelle on est pour réafficher le formulaire
-            $http->redirectTo('/booking');
+            //on crée une instance de la classe bookingForm
+            $bookingForm = new BookingForm();
+            //on lie les champs du bookingForm aux données de notre formulaire pour récuperer les données présentes dans les renseignés par l'utilisateur
+            $bookingForm->bind($formFields);
+            //puis on retourne les valeurs à la vue. Les valeurs retournées dans la methode get ci-dessus ont été retournées à nouveau parce qu'on ne fait pas de redirection
+            return [
+                "hoursList" => $bookingModel->displayHourList(),
+                "errorForm" => $message,
+
+                "_form" => $bookingForm
+            ];
+
+
 
         }
     }
