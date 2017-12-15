1a9ea22e18788627240e56a7dd10e5b9fb6509df
inconnu <exemple@dfg.com>
2017-12-15 12:49:40 +0100

    Database.class.php Ajout d'une ligne de code à ligne 29 pour afficher les erreurs de pdo

--- library/Database.class.php
+++ library/Database.class.php
@@ -26,6 +26,10 @@ class Database
 
 		$query->execute($values);
 
+		if ($query->errorCode() != '00000') {
+			throw new ErrorException($query->errorInfo()[2], $query->errorInfo()[1], 1, __FILE__, __LINE__);
+		}
+
 		return $this->pdo->lastInsertId();
 	}
 
