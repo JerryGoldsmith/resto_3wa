8e3451c2fda082b9603ccc6af8504ae275686faf
essentiel01 <tchekpo_cletus@yahoo.fr>
2017-12-17 20:53:54 +0100

    Creation de la classe HomeController qui dans sa methode httpGetMethod cree une instance de la classe MealModel puis recupre le resultat de la requete executee par la function findAllMeal.

--- application/controllers/HomeController.class.php
+++ application/controllers/HomeController.class.php
@@ -4,12 +4,11 @@ class HomeController
 {
     public function httpGetMethod(Http $http, array $queryFields)
     {
-    	/*
-    	 * Méthode appelée en cas de requête HTTP GET
-    	 *
-    	 * L'argument $http est un objet permettant de faire des redirections etc.
-    	 * L'argument $queryFields contient l'équivalent de $_GET en PHP natif.
-    	 */
+    	$allMeal = new MealModel(new Database());
+
+    	return [
+			"all"=>$allMeal->findAllMeal()
+		];
     }
 
     public function httpPostMethod(Http $http, array $formFields)
