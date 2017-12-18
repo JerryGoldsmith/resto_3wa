d19f48ae53051f0e28f48b255ff81ccfcb4634eb
inconnu <exemple@dfg.com>
2017-12-18 17:31:28 +0100

    Creation de la fonction getShoppingCartCount. Creation de la fonction displayShoppingCart. Creation de la fonction saveShoppingCart. Creation de la fonction clearShoppingCart. Ajout d'un test de verification de dollar session dans le contructeur et assignation d'une valeur par defaut a dollarshoppingCart.

--- application/models/PanierModel.class.php
+++ application/models/PanierModel.class.php
@@ -1,15 +1,41 @@
 <?php
 	class PanierModel extends AbstractModel {
-		protected $shoppingCart=[];
+
+		protected $shoppingCart;
+			protected $count;
 
 		public function __construct($db) {
 			parent::__construct($db);
+			if (!isset($_SESSION['panier'])){
+				$_SESSION['panier'] = [];
+			}
+			$this->shoppingCart = $_SESSION['panier'] ;
+
 		}
 
 		public function addToShoppingCart(array $selectedProduct){
-		array_push($this->shoppingCart,$selectedProduct);
-			$_SESSION["panier"] = $this->shoppingCart;
-			return $_SESSION["panier"];
+			$this->shoppingCart[] = $selectedProduct ;
+			 return $this;
+		}
+
+		public function getShoppingCartCount(){
+			return $this->count;
+		}
+
+		public function displayShoppingCart() {
+			return $this->shoppingCart;
+		}
+
+		public function saveShoppingCart()
+		{
+			$_SESSION['panier'] = $this->shoppingCart;
+			// return $this;
+		}
+
+		public function clearShoppingCart() {
+			$this->shoppingCart = array();
+			$this->saveShoppingCart();
+
 		}
 	}
 
