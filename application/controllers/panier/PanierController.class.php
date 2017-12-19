e411c9498ef7998068fe450d175b6abb06430115
inconnu <exemple@dfg.com>
2017-12-19 17:10:44 +0100

    Ajout du montant total, ht, ttc et tva. ajout de la suppression d'un élément isolé dans le panier.

--- application/controllers/panier/PanierController.class.php
+++ application/controllers/panier/PanierController.class.php
@@ -5,16 +5,28 @@ class PanierController
     public function httpGetMethod(Http $http, array $queryFields)
     {
         $session = new UserSession();
-
         $panier1 = new PanierModel(new Database());
 
         return [
-            "monPanier"=>$panier1->displayShoppingCart()
+            "monPanier"=>$panier1->displayShoppingCart(),
+            "montantTotal"=>$panier1->totalAmount(),
+            "tva"=>$panier1->tva(),
+            "ttc"=>$panier1->ttc()
         ];
     }
 
     public function httpPostMethod(Http $http, array $formFields)
     {
 
+        //Pour éviter de renvoyer une erreur de type undifined variable monPanier
+        $session = new UserSession();
+
+        $panier1 = new PanierModel(new Database());
+        $panier1->deleteOneProduct($formFields['mealId']);
+
+        return [
+            "monPanier"=>$panier1->displayShoppingCart(),
+            "montantTotal"=>$panier1->totalAmount()
+        ];
     }
 }
